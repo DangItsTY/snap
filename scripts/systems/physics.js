403a5218a9bfb31e0217692b5840f8f8f6ea0a38
Ty Dang <ty.dang1110@gmail.com>
2018-08-17 11:34:04 -0400

    fixed collision floor auto climb up

--- scripts/systems/physics.js
+++ scripts/systems/physics.js
@@ -14,16 +14,36 @@ function runPhysics(list) {
 	function stick(list) {
 		for (var i = 0; i < list.length; i++) {
 			var object = list[i];
-			if (object.vy > 0) {
-				//	do not apply gravity if object is colliding with a platform
-				for (var j = 0; j < object.collisions.length; j++) {
-					var target = object.collisions[j];
-					if (target.type == "platform") {
-						object.collisionFloor = target;
-						object.vy = 0;
-						object.y = target.y - (target.height / 2) - (object.height / 2);
+			var target = object.collisionFloor;
+			
+			//	first check if object is on a floor
+			var isOnFloor = 0;
+			for (var j = 0; j < object.collisions.length; j++) {
+				target = object.collisions[j];
+				if (target.type == "platform") {
+						isOnFloor += 1;
+				}
+			}
+			if (isOnFloor == 0) {
+				object.collisionFloor = null;
+			}
+			target = object.collisionFloor;
+			
+			if (target == null) {
+					if (object.vy > 0) {
+					//	do not apply gravity if object is colliding with a platform
+					for (var j = 0; j < object.collisions.length; j++) {
+						target = object.collisions[j];
+						if (target.type == "platform") {
+							object.collisionFloor = target;
+							object.vy = 0;
+							object.y = target.y - (target.height / 2) - (object.height / 2);
+						}
 					}
 				}
+			} else {
+				object.vy = 0;
+				object.y = target.y - (target.height / 2) - (object.height / 2);
 			}
 		}
 	}
