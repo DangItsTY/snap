79c5b91896d89ddd6c8bd5fcf1657529c0e96e1c
Ty Dang <ty.dang1110@gmail.com>
2018-08-26 16:13:56 -0400

    wrap wrapped up

--- scripts/render/render.js
+++ scripts/render/render.js
@@ -2,23 +2,23 @@ function render(list) {
 	for (var i = 0; i < list.length; i++) {
 		var object = list[i];
 		var element = object.element;
-		//element.style.left = object.x + "px";
-		//element.style.top = object.y + "px";
-		element.style.left = object.x - (object.width / 2) - CAMERA.offsetX + "px";
+		//element.style.left = object.x - (object.width / 2) + "px";
+		//element.style.top = object.y - (object.height / 2) + "px";
+		
 		//element.style.top = object.y - (object.height / 2) - CAMERA.offsetY + "px";
 		element.style.top = object.y - (object.height / 2) + "px";
-		
-		//	show the wrapping area
-		var wrapLength = (CAMERA.x + (CAMERA.viewWidth / 2)) - GAME_BOUNDARY;
-		if (wrapLength > 0) {
-			if (object.x <= wrapLength) {
+		if (object == PLAYER) {
+			element.style.left = "375px";
+		} else {
+			element.style.left = object.x - (object.width / 2) - CAMERA.offsetX + "px";
+			
+			//	show the wrapping area
+			var wrapLength = (CAMERA.x + (CAMERA.viewWidth / 2)) - GAME_BOUNDARY;
+			if (object.x - (object.width) <= wrapLength) {
 				element.style.left = CAMERA.viewWidth - wrapLength + object.x - (object.width / 2) + "px";
 			}
-		}
-		wrapLength = 0 - (CAMERA.x - (CAMERA.viewWidth / 2));
-		if (wrapLength > 0) {
-			if (object.x >= GAME_BOUNDARY - wrapLength) {
-				//element.style.left = wrapLength + object.x - (object.width / 2) + "px";
+			wrapLength = 0 - (CAMERA.x - (CAMERA.viewWidth / 2));
+			if (object.x + (object.width) >= GAME_BOUNDARY - wrapLength) {
 				element.style.left = 0 + wrapLength - (GAME_BOUNDARY - object.x) - (object.width / 2) + "px";
 			}
 		}
